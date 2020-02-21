fc8a81aa1fef4e6a4f54315a2f64551cd21570e9
ardalan@mobilefringe.com @ CodeCloud <ardalan@mobilefringe.com @ CodeCloud>
2020-02-21 14:59:42 -0500

    Vestar Template Test 1.0  /leasing.vue

---
+++ leasing.vue
@@ -0,0 +1,50 @@
+<template>
+    <div> <!-- for some reason if you do not put an outer container div this component template will not render -->
+        <loading-spinner v-if="!dataLoaded"></loading-spinner>
+        <transition name="fade">
+            <div v-if="dataLoaded" v-cloak>
+                <div class="inside_page_header" v-if="pageBanner" v-bind:style="{ background: 'linear-gradient(0deg, rgba(0,0,0,0.2), rgba(0,0,0,0.2)), #5d7e67 url(' + pageBanner.image_url + ') center center' }">
+                    <div class="main_container position_relative">
+                        <h2>Pop-up Specialty Leasing</h2>
+                    </div>
+                </div>
+                <div class="main_container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <breadcrumb></breadcrumb>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <!--<div v-if="main" v-html="main.body"></div>-->
+                            <p>Opening a new store can feel like a risk, but pop up leasing can help to create brand recognition with flexible short term leasing options. Pop up, Specialty Leasing or short-term leasing may go by various names but the end goal is the same. This pop up leasing concept is transforming the traditional rules of retail and redefining how shoppers interact with brands. So, whether you’re an new retailer or established brick-and-mortar eager to showcase a seasonal assortment, our prime shopping real estate, pop up leasing opportunities are designed to help you achieve your business goals.
+
+Whether your goal is to launch in a new market, reach a larger consumer audience or add an additional revenue stream, pop up leasing is short- term leasing option with long-term goals. Long Beach Towne Center is now seeking retail partnerships with premier local and national retail pop up operators.
+
+So, are you ready to “pop up” with us? Then let’s talk!</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="leasing_contact" v-if="leasingInfo" v-html="leasingInfo.body"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="location_image_container">
+                    <div class="location_image" v-if="pageImages" v-for="item in pageImages">
+                        <img :src="item.image_url" alt="" />   
+                    </div>
+                </div>
+            </div>
+        </transition>
+    </div>
+</template>
+
+<script>
+var _extends=Object.assign||function(e){for(var a=1;a<arguments.length;a++){var t=arguments[a];for(var n in t)Object.prototype.hasOwnProperty.call(t,n)&&(e[n]=t[n])}return e};define(["Vue","vuex"],function(e,a){return e.component("location-component",{template:template,data:function(){return{dataLoaded:!1,pageBanner:null,main:null,leasingInfo:null,leasingBooklet:null,pageImages:null}},created:function(){var e=this;this.loadData().then(function(a){var t=e.findRepoByName("Leasing Banner").images;e.pageBanner=null!=t?t[0]:{image_url:"//codecloud.cdn.speedyrails.net/sites/5e4ee7d66e6f641683480000/image/jpeg/1529532304000/insidebanner2.jpg"};var n=e.findRepoByName("Leasing Booklet");n&&(e.leasingBooklet=n.images[0].image_url);var r=e.findRepoByName("Leasing Images");r&&(e.pageImages=r.images),e.main=a[1].data,e.leasingInfo=a[1].data.subpages[0],e.dataLoaded=!0})},computed:_extends({},a.mapGetters(["property","repos","findRepoByName"])),methods:{loadData:function(){var e;return regeneratorRuntime.async(function(a){for(;;)switch(a.prev=a.next){case 0:return this.property.mm_host=this.property.mm_host.replace("http:",""),a.prev=1,a.next=4,regeneratorRuntime.awrap(Promise.all([this.$store.dispatch("getData","repos"),this.$store.dispatch("LOAD_PAGE_DATA",{url:this.property.mm_host+"/pages/laurasmall-pop-ups-specialty-leasing.json"})]));case 4:return e=a.sent,a.abrupt("return",e);case 8:a.prev=8,a.t0=a["catch"](1),console.log("Error loading data: "+a.t0.message);case 11:case"end":return a.stop()}},null,this,[[1,8]])}}})});
+</script>
